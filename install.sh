053baa242f012271027cecc72eb4d420ddb617f8
Andrei Cioara <andrei@cioara.me>
2018-02-17 20:05:55 +0000

    Completed the install shell wrapper and added install python script

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 
+THIS_DIR=$(dirname "$0")
+python -u ${THIS_DIR}/src/install.py $@
---
+++ src/install.py
@@ -0,0 +1,10 @@
+#/usr/bin/env python
+
+import sys
+
+def main():
+    print "Hello World"
+    print sys.argv
+
+if __name__ == "__main__":
+    main()
